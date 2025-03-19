93d6b15b471dd5bd41290d09c5e5d2954d409c63
doniset625 <doniset625@gmail.com>
2025-03-19 14:43:14 +0700

    fix normalizedPath function in Router

--- src/Framework/Router.php
+++ src/Framework/Router.php
@@ -18,6 +18,7 @@ class Router{
     private function normalizePath(string $path): string{
         $path = trim($path, '/');
         $path = "/{$path}/";
+        $path = preg_replace('#[/]{2,}#','/',$path);
         return $path;
     }
 }
